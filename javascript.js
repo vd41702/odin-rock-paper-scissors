89bc2efb8b0c24cc283245bd27c20490f23572b1
Vinay Devareddy <vinaydevareddy1702@gmail.com>
2022-12-19 15:37:27 -0500

    add rps logic to js file

--- javascript.js
+++ javascript.js
@@ -1,33 +1,66 @@
 const buttons = document.querySelectorAll(".container>button");
+const compChoiceDiv = document.querySelector(".results > .selection");
+
+const compChoiceH4 = document.createElement('h4');
+const playerScoreDisplay = document.querySelector('#playerScore > p');
+const compScoreDisplay = document.querySelector('#computerScore > p');
+
+compChoiceDiv.appendChild(compChoiceH4);
 
 for(const btn of buttons) {
     btn.addEventListener('click', playRound);
 }
 
+playerScore = 0;
+computerScore = 0;
 
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random()*3)];
 }
 
-function playRound(e) {
-    console.log(e.target.textContent);
-    var cs = getComputerChoice();
+function calcRound(ps, cs) {
+    
     const choices = "RPS";
-    return;
     var c = choices.indexOf(cs.charAt(0).toUpperCase());
     var p = choices.indexOf(ps.charAt(0).toUpperCase());
 
     if(p == c) { //Tie
-        console.log(`A tie! Both players chose ${cs}`);
         return 0;
     } else if(p == c + 1 || p == c - 2) { //player wins
-        console.log(`You won! ${ps} beats ${cs}`);
         return 1;
     } else { //computer won
-        console.log(`The Computer won! ${cs} beats ${ps}`);
         return -1;
     }
+}
+
+function playRound(e) {
+    var ps = e.target.textContent;
+    var cs = getComputerChoice();
+    res = calcRound(ps, cs);
+
+    //check who won, and update scores
+    if(res == -1) {
+        computerScore++;
+    } else {
+        playerScore += res;
+    }
+
+    //display scores
+    playerScoreDisplay.innerText = playerScore;
+    compScoreDisplay.innerText = computerScore;
+
+    //display computer selection
+    compChoiceH4.innerText = cs;
+
+    //check if someone won
+    if(computerScore == 5) {
+        alert("You lost :(");
+    } else if(playerScore == 5) {
+        alert("You won!!");
+    }
+    
+    
 }
 
 
